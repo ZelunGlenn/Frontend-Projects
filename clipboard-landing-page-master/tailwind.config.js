dc172b3701a93ae79ce1f9154875c44d85ad550e
Zelun Zhang <121286574+ZelunGlenn@users.noreply.github.com>
2024-07-04 11:11:17 +0930

    hero section desktop part finished

--- clipboard-landing-page-master/tailwind.config.js
+++ clipboard-landing-page-master/tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        "Strong-Cyan": "hsl(171, 66%, 44%)",
+        "Light-Blue": "hsl(233, 100%, 69%)"
+      },
+      fontFamily: {
+        'Bai-Jamjuree': ['"Bai Jamjuree"', 'sans-serif'],
+      },
       screens: {
         "sm": "375px",
+      },
+      spacing: {
+        "375": "23.44rem",
+        "1440": "90rem",
       },
       backgroundImage: {
         "header-desktop": "url('./images/bg-header-desktop.png')",
